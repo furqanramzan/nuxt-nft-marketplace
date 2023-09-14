82a31af14e262cda636fdcf9697a886f8a6857fb
Muhammad Furqan <furqanramzan271996@gmail.com>
2023-09-14 17:48:12 +0500

    feat(flowbite): add flowbite vue

--- app.vue
+++ app.vue
@@ -1,7 +1,12 @@
+<script setup>
+import { Badge } from 'flowbite-vue';
+</script>
+
 <template>
-  <div class="flex h-screen w-screen items-center justify-center">
+  <div class="flex h-screen w-screen flex-col items-center justify-center">
     <h1 class="text-2xl font-bold text-primary-600 sm:text-4xl lg:text-8xl">
       Nuxt NFT Marketplace
     </h1>
+    <Badge type="green">Built with Nuxt3</Badge>
   </div>
 </template>
