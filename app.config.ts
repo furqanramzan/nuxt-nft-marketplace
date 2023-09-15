df347ef8634df7e5ff2c0cad58d5bdbfbd873d5a
Muhammad Furqan <furqanramzan271996@gmail.com>
2023-09-15 15:22:09 +0500

    feat(config): add app config and fix nuxt icon type issue

---
+++ app.config.ts
@@ -0,0 +1,7 @@
+export default defineAppConfig({
+  nuxtIcon: {
+    size: '24px',
+    class: 'icon',
+    aliases: {},
+  },
+});
--- components/DarkMode.vue
+++ components/DarkMode.vue
@@ -21,7 +21,7 @@ onMounted(() => {
     className="flex h-14 w-14 items-center justify-center rounded-full bg-gray-200 text-sm text-gray-500 shadow-md hover:bg-gray-100 focus:outline-none focus:ring-4 focus:ring-gray-300 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-700"
     @click="toggleTheme"
   >
-    <Icon v-if="isDark" name="material-symbols:light-mode" size="24px" />
-    <Icon v-else name="material-symbols:dark-mode" size="24px" />
+    <Icon v-if="isDark" name="material-symbols:light-mode" />
+    <Icon v-else name="material-symbols:dark-mode" />
   </button>
 </template>
